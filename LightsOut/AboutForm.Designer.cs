228368d43bcd028cc7261a04ef9320d08af14ff5
CJ Wilson <cwilson14@harding.edu>
2017-08-30 15:53:25 -0500

    Minor fixes from Easel feedback.
    Tiles at new game are turned on and off randomly, project icon set, close button added to about menu.

--- LightsOut/AboutForm.Designer.cs
+++ LightsOut/AboutForm.Designer.cs
@@ -60,6 +60,7 @@
 			// 
 			// button1
 			// 
+			this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
 			this.button1.Location = new System.Drawing.Point(198, 167);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
@@ -93,7 +94,6 @@
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(449, 202);
-			this.ControlBox = false;
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.pictureBox1);
 			this.Controls.Add(this.label3);
@@ -101,6 +101,8 @@
 			this.Controls.Add(this.label2);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
 			this.Name = "AboutForm";
 			this.ShowIcon = false;
 			this.ShowInTaskbar = false;
